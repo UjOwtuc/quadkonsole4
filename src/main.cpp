3ee2bd4ed36c4a4f3b90de0a8276cde7050e0060
Karsten Borgwaldt <kb@spambri.de>
2021-02-15 19:32:32 +0100

    Fix command line help text

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *   Copyright (C) 2005 by Simon Perreault                                 *
  *   nomis80@nomis80.org                                                   *
- *   Copyright (C) 2009 - 2017 by Karsten Borgwaldt                        *
+ *   Copyright (C) 2009 - 2021 by Karsten Borgwaldt                        *
  *   kb@spambri.de                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -35,8 +35,6 @@
 #error QUADKONSOLE4_VERSION undefined
 #endif
 
-static const char description[] = I18N_NOOP("Embeds multiple Konsoles in a grid layout");
-static const char version[] = QUADKONSOLE4_VERSION;
 
 int main(int argc, char* argv[])
 {
@@ -44,7 +42,10 @@ int main(int argc, char* argv[])
 
 	KLocalizedString::setApplicationDomain("quadkonsole4");
 
-	KAboutData about(QStringLiteral("quadkonsole4"), i18n("QuadKonsole4"), version, description, KAboutLicense::GPL_V2, i18n("(C) 2005 Simon Perreault\n(C) 2009 - 2017 Karsten Borgwaldt"), "", QStringLiteral("http://spambri.de/quadkonsole4"), QStringLiteral("quadkonsole4@spambri.de"));
+	KAboutData about(QStringLiteral("quadkonsole4"), i18n("QuadKonsole4"), QStringLiteral(QUADKONSOLE4_VERSION),
+			 i18n("Embeds multiple Konsoles in a grid layout"),
+			 KAboutLicense::GPL_V2, i18n("(C) 2005 Simon Perreault\n(C) 2009 - 2021 Karsten Borgwaldt"), "",
+			 QStringLiteral("http://spambri.de/quadkonsole4"), QStringLiteral("quadkonsole4@spambri.de"));
 	about.addAuthor(i18n("Simon Perreault"), "", QStringLiteral("nomis80@nomis80.org"));
 	about.addAuthor(i18n("Karsten Borgwaldt"), "", QStringLiteral("kb@spambri.de"));
 	about.addCredit(i18n("Michael Feige"), i18n("Many ideas and feature requests"));
@@ -56,8 +57,8 @@ int main(int argc, char* argv[])
 
 	parser.addOption(QCommandLineOption(QStringList() << "r" << "rows", i18n("Number of rows of terminal emulators"), i18n("rows"), QString::number(2)));
 	parser.addOption(QCommandLineOption(QStringList() << "c" << "columns", i18n("Number of columns of terminal emulators"), i18n("columns"), QString::number(2)));
-	parser.addOption(QCommandLineOption(QStringList() << "C" << "cmd" << i18n("Run command [in given view] (may be used multiple times)")));
-	parser.addOption(QCommandLineOption(QStringList() << "u" << "url" << i18n("Open URL [in given view] (may be used multiple times)")));
+	parser.addOption(QCommandLineOption(QStringList() << "C" << "cmd", i18n("Run command [in given view] (may be used multiple times)"), i18n("command")));
+	parser.addOption(QCommandLineOption(QStringList() << "u" << "url", i18n("Open URL [in given view] (may be used multiple times)"), i18n("URL")));
 	parser.addPositionalArgument(i18n("URL"), i18n("Open specified URL in a running instance of QuadKonsole4"));
 	about.setupCommandLine(&parser);
 	parser.process(app);
